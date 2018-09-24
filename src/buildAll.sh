005ad4bef9c7ea7ddf96bc5f563ade711e6c3df8
Ehsan Sadrfaridpour <esadrfa@g.clemson.edu>
2018-09-24 14:55:31 -0400

    fix multiple makefiles for FLANN

--- src/buildAll.sh
+++ src/buildAll.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+
 echo "Make all the tools and main application from source code"
 make clean
 make mlsvm_save_knn 
-make mlsvm_csv_petsc 
-make mlsvm_knn 
+make mlsvm_csv_petsc
+
+# This is required for FLANN installation path which are different on HPC
+out=`hostname | grep paliametto | wc -l` 
+if [ $out -eq 1 ] 
+then
+  #echo "This is on the Palmetto Cluster"
+  make mlsvm_knn -f Makefile_palmetto 
+else
+  make mlsvm_knn 
+fi
+
 make mlsvm_zscore 
 make mlsvm_save_knn 
 make mlsvm_predict 
