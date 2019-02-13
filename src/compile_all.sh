808f5b81f2b19d32a1e472bd9b358f7a81e73736
Ehsan SadrFaridpour <esadr@users.noreply.github.com>
2019-02-13 12:31:29 -0500

    Update compile_all.sh

--- src/compile_all.sh
+++ src/compile_all.sh
@@ -1,5 +1,9 @@
-make mlsvm_csv_petsc
-make mlsvm_zscore   
-make mlsvm_knn           
-make 
+#!/bin/bash 
 
+make mlsvm_csv_petsc
+make mlsvm_libsvm_petsc
+make mlsvm_knn
+make mlsvm_zscore
+make mlsvm_classifier
+make mlsvm_predict
+echo "All required modules are compiled successfully!"
